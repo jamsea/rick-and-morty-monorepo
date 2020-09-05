65c4429874b85f6d22fd0b67751bb929c57a40c7
psmith <psmith.code@gmail.com>
2020-09-05 13:22:21 -0700

    added character page

---
+++ sites/gatsby-rick-and-morty/src/pages/character.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const CharacterPage = () => (
+  <>
+    <SEO title="Character Page" />
+    <h1>I'm a character page</h1>
+  </>
+)
+
+export default CharacterPage

ed6e1fa8f7411afdca91fa968fccb9e1500bb741
James Hush <jhush91@gmail.com>
2020-09-05 14:13:07 -0700

    feat: create hardcoded character page

--- sites/gatsby-rick-and-morty/src/pages/character.tsx
+++ sites/gatsby-rick-and-morty/src/pages/character.tsx
@@ -1,11 +1,27 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
 
-const CharacterPage = () => (
+const CharacterPage = ({ data }) => (
   <>
     <SEO title="Character Page" />
-    <h1>I'm a character page</h1>
+    <h1>{data.rickAndMorty.characters.results[0].name}</h1>
+    <img src={data.rickAndMorty.characters.results[0].image} />
   </>
 )
+
+export const query = graphql`
+  query CharacterPageQuery {
+    rickAndMorty {
+      characters(page: 1, filter: { name: "poopy" }) {
+        results {
+          name
+          image
+        }
+      }
+    }
+  }
+`
 
 export default CharacterPage

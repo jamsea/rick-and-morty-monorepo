d7c6cd763d6538990e3277a6ecf400c9df761dc1
psmith <psmith.code@gmail.com>
2020-09-05 16:57:20 -0700

    variables in gql work

--- sites/gatsby-rick-and-morty/src/pages/character.tsx
+++ sites/gatsby-rick-and-morty/src/pages/character.tsx
@@ -1,27 +1,52 @@
-import React from "react"
+import * as React from "react"
+import { useEffect } from "react"
+import { PageProps } from "gatsby"
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
+import queryString from "query-string"
 import SEO from "../components/seo"
 
-const CharacterPage = ({ data }) => (
-  <>
-    <SEO title="Character Page" />
-    <h1>{data.rickAndMorty.characters.results[0].name}</h1>
-    <img src={data.rickAndMorty.characters.results[0].image} />
-  </>
-)
+const CharacterPage = ({ location }: PageProps) => {
+  console.log("hi")
 
-export const query = graphql`
-  query CharacterPageQuery {
-    rickAndMorty {
-      characters(page: 1, filter: { name: "poopy" }) {
-        results {
-          name
-          image
+  useEffect(() => {
+    const queriedCharacter = queryString.parse(location.search)
+    console.log("queriedCharacter", queriedCharacter)
+    const searchString = queriedCharacter.q
+    console.log("character", searchString)
+
+    const query = graphql`
+      query CharacterPageQuery {
+        rickAndMorty {
+          characters(page: 1, filter: { name: $searchString }) {
+            results {
+              name
+              image
+            }
+          }
         }
       }
-    }
-  }
-`
+    `
+
+    const url = "https://rickandmortyapi.com/graphql"
+
+    const requestHeaders: HeadersInit = new Headers()
+    requestHeaders.set("Content-Type", "application/json")
+    const response = fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      headers: requestHeaders,
+      body: JSON.stringify({ query }),
+    })
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.error)
+  }, [])
+
+  return (
+    <>
+      <h1></h1>
+    </>
+  )
+}
 
 export default CharacterPage

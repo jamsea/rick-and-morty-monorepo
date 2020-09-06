5ab8758384349acf434457ce028fb89755c862fd
pricesmith <62907266+pricesmith@users.noreply.github.com>
2020-09-05 17:34:12 -0700

    Price/graphql stuff (#5)
    * variables in gql work
    
    * fetch works
    
    * it works
    
    Co-authored-by: James Hush <jhush91@gmail.com>

--- sites/gatsby-rick-and-morty/src/pages/character.tsx
+++ sites/gatsby-rick-and-morty/src/pages/character.tsx
@@ -1,36 +1,56 @@
-import React from "react"
-import { graphql, PageProps } from "gatsby"
-import Img from "gatsby-image"
+import * as React from "react"
+import { useEffect, useState } from "react"
+import { PageProps } from "gatsby"
+import { graphql } from "gatsby"
+import queryString from "query-string"
 import SEO from "../components/seo"
 
-import { Query } from "../graphqlTypes"
+const CharacterPage = ({ location }: PageProps) => {
+  const [name, setName] = useState("")
+  const [imageUrl, setImageUrl] = useState("")
 
-const CharacterPage = ({ data }: PageProps<Query>) => {
-  const results = data.rickAndMorty?.characters?.results || [
-    { name: "", image: "" },
-  ]
-  const { name, image } = results[0]
+  useEffect(() => {
+    const queriedCharacter = queryString.parse(location.search)
+    console.log("queriedCharacter", queriedCharacter)
+    const searchString = queriedCharacter.q
+    console.log("character", searchString)
+
+    const query = `
+        query {
+          characters(page: 1, filter: { name: "${searchString}" }) {
+          results {
+            name
+            image
+          }
+        }
+      }
+    `
+
+    const url = "https://rickandmortyapi.com/graphql"
+
+    const requestHeaders: HeadersInit = new Headers()
+    requestHeaders.set("Content-Type", "application/json")
+    console.log("query: ", query)
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: query }),
+    })
+      .then(res => res.json())
+      .then(r => {
+        console.log(r.data.characters.results[0].name)
+        setName(r.data.characters.results[0].name)
+        setImageUrl(r.data.characters.results[0].image)
+      })
+      .catch(console.error)
+  }, [])
 
   return (
     <>
-      <SEO title="Character Page" />
       <h1>{name}</h1>
-      <img src={image} />
+      <img src={imageUrl} />
     </>
   )
 }
-
-export const query = graphql`
-  query CharacterPageQuery {
-    rickAndMorty {
-      characters(page: 1, filter: { name: "poopy" }) {
-        results {
-          name
-          image
-        }
-      }
-    }
-  }
-`
 
 export default CharacterPage
